07a79658d2901cbe47372d467177a970fda156ae
JonHodgins <jon_hodgins@outlook.com>
2018-05-24 17:40:07 -0700

    Finished Nodejs/MySQL based web app

--- web_app_final_project/SpicySignup/app.js
+++ web_app_final_project/SpicySignup/app.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var mysql = require('mysql');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -8,19 +13,29 @@ var connection = mysql.createConnection({
     database: 'spicy_signup'
 });
 
-
-app.get("/", function(req, res){
+app.get('/', function(req, res) {
     var q = 'SELECT Count(*) AS count FROM users';
     connection.query(q, function(error, results) {
-    if (error) throw error;
-    res.send("There are " + results[0].count + " users in the database");
+        if (error) throw error;
+        var count = results[0].count;
+        res.render('home', {data: count});
     });
 });
 
-app.get("/joke", function(req, res){
+app.post('/register', function(req, res){
+    var person = {
+        email: req.body.email
+    };
+    connection.query('INSERT INTO users SET ?', person, function(error, results) {
+        if (error) throw error;
+        res.redirect('/');
+    });
+});
+
+app.get('/joke', function(req, res) {
     res.send("You can't run through a camp site. You can only ran, because it's <strong>past tents.</strong>")
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Server running on port " + process.env.PORT);
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log('Server running on port ' + process.env.PORT);
 });
